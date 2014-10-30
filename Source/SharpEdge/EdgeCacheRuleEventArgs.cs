40a7b934284659e175501032be1164268fc3c96e
Keith <Keith@localhost>
2014-10-30 13:10:06 +0000

    Added rule event args

---
+++ Source/SharpEdge/EdgeCacheRuleEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Web;
+
+namespace SharpEdge
+{
+	public class EdgeCacheRuleEventArgs : EdgeCacheEventArgs
+	{
+		private EdgeCacheRule _rule;
+
+		public EdgeCacheRuleEventArgs(HttpContext context, EdgeCacheRule rule)
+			: base(context)
+		{
+			_rule = rule;
+		}
+
+		public EdgeCacheRule Rule
+		{
+			get
+			{
+				return _rule;
+			}
+		}
+	}
+}
